d6fb58eba2cc5663882fada1434066a84c5d91f1
JudyKamande <judy.kamande@gmail.com>
2015-06-16 11:32:48 +0300

    Update main.c

--- example1/main.c
+++ example1/main.c
@@ -1,4 +1,4 @@
-// Example1: C-Program
+// Example 1: C-Program
 
 #include <stdio.h>  //preprocessor directive to include Input/Output Standard C library file
 #include <stdlib.h> //preprocessor directive to include Standard C library file for Text -to/from- Number conversion, Number manipulations, Memory allocation...etc
@@ -9,7 +9,7 @@ int main (int argc, char* argv[])
 {
 	char a = atoi(argv[1]); //variable declaration for 'a'
 	printf("The value of a was %d \n", a); //output on-screen value passed //Format Control String  
-	// NOTE: Ampersand & ommitted due to printf Statement - does not store to memory but reads from memory
+	// NOTE: Ampersand & ommitted due to printf Statement - does not store to memory but reads from memory address
 	return 0;
 }
 //End of function Main

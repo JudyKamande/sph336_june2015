910c630051b9ee0da9455c145673680b7a73f2c0
JudyKamande <judy.kamande@gmail.com>
2015-06-16 11:26:10 +0300

    Update main.c

--- example1/main.c
+++ example1/main.c
@@ -1,7 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main (int argc, char* argv[]){
-	char a = atoi(argv[1]);
-	printf("the value of a was %d\n", a);
+// Example1: C-Program
+
+#include <stdio.h>  //preprocessor directive to include Input/Output Standard C library file
+#include <stdlib.h> //preprocessor directive to include Standard C library file for Text -to/from- Number conversion, Number manipulations, Memory allocation...etc
+
+//Beginning function Main - which returns integer 
+int main (int argc, char* argv[]) 
+//Arguments in parentheses passed by value to function Main
+{
+	char a = atoi(argv[1]); //variable declaration for 'a'
+	printf("The value of a was %d \n", a); //output on-screen value passed //Format Control String
 	return 0;
 }
+//End of function Main

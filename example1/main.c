d7f037d48963cd39e1b8500c392fc3acb41170e4
JudyKamande <judy.kamande@gmail.com>
2015-06-16 11:31:23 +0300

    Update main.c

--- example1/main.c
+++ example1/main.c
@@ -8,7 +8,8 @@ int main (int argc, char* argv[])
 //Arguments in parentheses passed by value to function Main
 {
 	char a = atoi(argv[1]); //variable declaration for 'a'
-	printf("The value of a was %d \n", a); //output on-screen value passed //Format Control String
+	printf("The value of a was %d \n", a); //output on-screen value passed //Format Control String  
+	// NOTE: Ampersand & ommitted due to printf Statement - does not store to memory but reads from memory
 	return 0;
 }
 //End of function Main
